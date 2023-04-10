03e2b88b6f09177fc82c08344b2ed29e6ddf03bc
Rexios <rexios@rexios.dev>
2023-04-10 14:40:52 -0400

    Initial commit

---
+++ bin/git_https_to_ssh.dart
@@ -0,0 +1,5 @@
+import 'package:git_https_to_ssh/git_https_to_ssh.dart' as git_https_to_ssh;
+
+void main(List<String> arguments) {
+  print('Hello world: ${git_https_to_ssh.calculate()}!');
+}
---
+++ test/git_https_to_ssh_test.dart
@@ -0,0 +1,8 @@
+import 'package:git_https_to_ssh/git_https_to_ssh.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
